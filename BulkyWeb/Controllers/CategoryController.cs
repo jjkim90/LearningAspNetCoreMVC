7276b8f33a2d518b6e310fe08628a4d09a7d7922
jjkim90 <jaejun8613@gmail.com>
2024-02-25 15:42:00 +0900

    Add custom validation on category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -25,6 +25,14 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if(obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value.");
+            }
             if(ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
