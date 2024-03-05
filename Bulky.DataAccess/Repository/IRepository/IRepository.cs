9cc13cc1fa93b426cb45bebd78c0006978a09ae7
jjkim90 <jaejun8613@gmail.com>
2024-03-05 22:24:39 +0900

    Add Repository implementation

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         //T - Category
         IEnumerable<T> GetAll();
-        T Get(Expression<Func<T, bool>> filter);
+        T? Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
