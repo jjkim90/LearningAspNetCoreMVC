3f1ce0f9108c1ed0720fb15ce4c549e7c5165128
jjkim90 <jaejun8613@gmail.com>
2024-03-07 21:14:50 +0900

    Change solution name and projects names

---
+++ BookStack.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookStack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStack.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
---
+++ BookStack.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStack.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; } = string.Empty;
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "The field Display Order must be between 1-100.")]
+        public int DisplayOrder { get; set; }
+    }
+}
